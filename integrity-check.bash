1d04af568465496e84bcbc9143aa93d8c117c32b
mccarrmb <mccarrmb@gmail.com>
2019-07-05 22:15:23 -0400

    added check stubs

--- integrity-check.bash
+++ integrity-check.bash
@@ -1,3 +1,58 @@
 #!/bin/bash
 
+# Get list of changed files in this branch
+git diff --name-only master `git rev-parse --abbrev-ref HEAD`
+
 find . -type f -name "*.sql"
+
+
+
+
+function check_mysql {
+
+}
+
+function check_postgresql {
+
+}
+
+function check_csv {
+
+}
+
+function check_xml {
+    
+}
+
+function check_scala {
+    
+}
+
+function check_java {
+    
+}
+
+function check_ruby {
+    
+}
+
+function check_perl {
+    
+}
+
+function check_nodejs {
+    
+}
+
+function check_javascript {
+    
+}
+
+function check_python {
+    
+}
+
+function check_cron {
+    
+}
+
