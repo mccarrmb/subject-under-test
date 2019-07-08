ecaca59e8a1c160b7a6aa212e63b7743bbce8c21
mccarrmb <mccarrmb@gmail.com>
2019-07-07 23:37:54 -0400

    travis fix

--- integrity-check.bash
+++ integrity-check.bash
@@ -2,11 +2,13 @@
 export FAILURE=0
 
 function csv_checks {
+  echo -e "\e[1;33mChecking $1...\e[0m"
   csvlint $1
   if [ $? -gt 0 ] ; then
-    echo "$1 failed check."
+    echo -e "\e[1;31m$1 failed check.\e[0m"
     FAILURE=1
   fi
+  echo -e "\e[1;33mCheck complete.\e[0m"
 }
 
 # Get diff of changed files in this branch compared to master
@@ -15,15 +17,12 @@ current_branch=`git rev-parse --abbrev-ref HEAD`
 modified_files=`git diff --name-only $current_branch master`
 
 # Run checks over modified file blob
-for file in $modified_files; do
-    echo "Checking $file..."
-    
+for file in $modified_files; do    
     # CSV file
     if [[ $file == *.csv ]] ; then
       csv_checks "$file"
     fi
 
-    echo "Check complete."
 done
 
 exit $FAILURE
