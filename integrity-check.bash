5c0de37800db15992eb7c02b8a69d82cec93afa9
Whitey McCarragher <matt.mccarragher@online-rewards.com>
2019-07-08 16:01:39 -0400

    broke xml

--- integrity-check.bash
+++ integrity-check.bash
@@ -3,7 +3,18 @@ export FAILURE=0
 
 function csv_checks {
   echo -e "\e[1;33mChecking $1...\e[0m"
-  csvlint $1
+  csvlint "$1"
+  if [ $? -gt 0 ] ; then
+    echo -e "\e[1;31m$1 failed check.\e[0m"
+    FAILURE=1
+  else
+    echo -e "\e[1;32m$1 passed checks.\e[0m"
+  fi
+}
+
+function xml_checks {
+  echo -e "\e[1;33mChecking $1...\e[0m"
+  xmllint "$1"
   if [ $? -gt 0 ] ; then
     echo -e "\e[1;31m$1 failed check.\e[0m"
     FAILURE=1
@@ -20,16 +31,12 @@ modified_files=`git diff --name-only $current_branch master`
 # Run checks over modified file blob
 for file in $modified_files; do    
     # CSV file
-    if [[ $file == *.csv ]] ; then
+    if [[ "$file" == *.csv ]] ; then
       csv_checks "$file"
+    elif [[ "$file" == *.xml || "$file" == *.xsd || "$file" == *.xlst ]]; then
+      xml_checks "$file"
     fi
 
 done
 
 exit $FAILURE
-
-#find . -type f -name "*."
-    # sudo apt install golang-go
-    # export PATH="$PATH:$HOME/go/bin"
-    # go get github.com/Clever/csvlint/cmd/csvlint
-    # csvlint [loop over blob]
