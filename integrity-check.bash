d178bdf1dc07fa2cc4c01ec465f2778e7e2fb5c3
Whitey McCarragher <matt.mccarragher@online-rewards.com>
2019-07-08 16:34:48 -0400

    deleted file test2 - take 3

--- integrity-check.bash
+++ integrity-check.bash
@@ -31,10 +31,10 @@ current_branch=`git rev-parse --abbrev-ref HEAD`
 modified_files=`git diff --diff-filter=CMART --name-only $current_branch master`
 
 # Run checks over modified file blob
-for file in $modified_files; then
+for file in $modified_files; do
   # Since git diff is comparing to master, a file technically counts as being added 
   # even if it is added and then subsequently deleted in this branch.
-  if [ -f "$file" ]; do
+  if [ -f "$file" ]; then
     # CSV file
     if [[ "$file" == *.csv ]] ; then
       csv_checks "$file"
