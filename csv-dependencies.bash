5683f24c17bd54ed7393ecfd18100001d7fd2764
mccarrmb <mccarrmb@gmail.com>
2019-07-07 23:08:04 -0400

    travis fix

--- csv-dependencies.bash
+++ csv-dependencies.bash
@@ -2,4 +2,4 @@
 csvlint_version='csvlint-v0.2.0-linux-amd64'
 wget "https://github.com/Clever/csvlint/releases/download/0.2.0/$csvlint_version.tar.gz"
 tar xvzf "$csvlint_version.tar.gz"
-sudo mv "$csvlint_version/csvlint" "/usr/local/sbin/"
+mv "$csvlint_version/csvlint" "./"
