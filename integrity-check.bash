00533f4feff6de28d2f87a83dc01117bf359f23f
mccarrmb <mccarrmb@gmail.com>
2019-07-07 22:01:39 -0400

    better checks

--- integrity-check.bash
+++ integrity-check.bash
@@ -1,4 +1,13 @@
 #!/bin/bash
+export FAILURE=0
+
+function csv_checks {
+  csvlint $1
+  if [ $? -gt 0 ] ; then
+    echo "$1 failed check."
+    FAILURE=1
+  fi
+}
 
 # Get diff of changed files in this branch compared to master
 current_branch=`git rev-parse --abbrev-ref HEAD`
@@ -7,17 +16,19 @@ modified_files=`git diff --name-only origin/master $current_branch`
 # Run checks over modified file blob
 for file in $modified_files; do
     echo "Checking $file..."
+    
+    # CSV file
     if [[ $file == *.csv ]] ; then
-      csvlint "$file"
+      csv_checks "$file"
     fi
+    
     echo "Check complete."
 done
+
+exit $FAILURE
 
 #find . -type f -name "*."
     # sudo apt install golang-go
     # export PATH="$PATH:$HOME/go/bin"
     # go get github.com/Clever/csvlint/cmd/csvlint
     # csvlint [loop over blob]
-function csv_checks {
-  csvlint $1
-}
