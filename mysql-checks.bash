318889fa0f6c49c8301601198941e5c1b9c6f068
Whitey McCarragher <matt.mccarragher@online-rewards.com>
2019-07-15 21:16:07 -0400

    dammit mysql

--- mysql-checks.bash
+++ mysql-checks.bash
@@ -5,8 +5,12 @@ export FAILURE=0
 function mysql_check() {
   lint_result=`mysql -uroot-h127.0.0.1 mysql_test -f -b < $1 2>&1`;
   if [ "`echo $lint_result | sed -r \"s/ERROR ([0-9]*).*/\1/g\"`" = "1064" ]; then
+    echo -e "\e[1;31m$1 failed check.\e[0m"
     FAILURE=1
+  else
+    echo -e "\e[1;32m$1 passed checks.\e[0m"
   fi;
+  
 }
 
 # Get diff of changed files in this branch compared to master
@@ -21,13 +25,7 @@ for file in $modified_files; do
   if [[ -f "$file" && "$file" == mysql/*.sql ]]; then
     echo -e "\e[1;33mChecking $file...\e[0m"
     # Perl compile only
-    mysql_check "$file" 1>/dev/null
-    if [ $? -gt 0 ]; then
-      echo -e "\e[1;31m$file failed check.\e[0m"
-      FAILURE=1
-    else
-      echo -e "\e[1;32m$file passed checks.\e[0m"
-    fi
+    mysql_check "$file"
   fi
 done
 
