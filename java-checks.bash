36a0c5bbcecde388252ac9d128835b94a24d46e6
Whitey McCarragher <matt.mccarragher@online-rewards.com>
2019-07-15 20:33:29 -0400

    dammit java

--- java-checks.bash
+++ java-checks.bash
@@ -1,28 +1,15 @@
 #!/bin/bash
 export FAILURE=0
-
-# Get diff of changed files in this branch compared to master
-git fetch origin master
-current_branch=`git rev-parse --abbrev-ref HEAD`
-modified_files=`git diff --diff-filter=CMART --name-only master $current_branch`
-
 export CLASSPATH="./java;$CLASSPATH"
 
-# Run checks over modified file blob
-for file in $modified_files; do
-  # Since git diff is comparing to master, a file technically counts as being added 
-  # even if it is added and then subsequently deleted in this branch.
-  if [[ -f "$file" && ( "$file" == *.java || "$file" == *.jsp ) ]]; then
-    echo -e "\e[1;33mChecking $file...\e[0m"
-    # Java compiler
-     javac "$file" 1>/dev/null
-    if [ $? -gt 0 ]; then
-      echo -e "\e[1;31m$file failed check.\e[0m"
-      FAILURE=1
-    else
-      echo -e "\e[1;32m$file passed checks.\e[0m"
-    fi
-  fi
-done
+echo -e "\e[1;33mChecking Java project...\e[0m"
+# Java compiler
+javac java/*.java 1>/dev/null
+if [ $? -gt 0 ]; then
+  echo -e "\e[1;31m$file failed check.\e[0m"
+  FAILURE=1
+else
+  echo -e "\e[1;32m$file passed checks.\e[0m"
+fi
 
 exit $FAILURE
