2285a68c39fad1ad2b315ca03ed59758853e474f
Whitey McCarragher <matt.mccarragher@online-rewards.com>
2019-07-15 20:36:12 -0400

    dammit java

--- java-checks.bash
+++ java-checks.bash
@@ -5,10 +5,10 @@ echo -e "\e[1;33mChecking Java project...\e[0m"
 # Java compiler
 javac java/*.java 1>/dev/null
 if [ $? -gt 0 ]; then
-  echo -e "\e[1;31m$file failed check.\e[0m"
+  echo -e "\e[1;31mJava project failed check.\e[0m"
   FAILURE=1
 else
-  echo -e "\e[1;32m$file passed checks.\e[0m"
+  echo -e "\e[1;32mJava project passed checks.\e[0m"
 fi
 
 exit $FAILURE
